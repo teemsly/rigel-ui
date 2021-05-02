14926e2d689dec1ad4c425bc984afe3882c69672
mrsepdiana <firdasepdiana@gmail.com>
2021-05-02 22:41:23 +0700

    register addClassPrefix, createComponent, and useClassName

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,1 +1,4 @@
+export { default as addClassPrefix } from "./addClassPrefix";
 export * from "./common";
+export { default as createComopnent } from "./createComponent";
+export { default as useClassName } from "./useClassName";

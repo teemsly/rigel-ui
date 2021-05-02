1b06e58b1500cf6b47fd7337b60bbd82a4ef59ca
mrsepdiana <firdasepdiana@gmail.com>
2021-05-02 22:50:59 +0700

    remove unnecessary variables

--- src/utils/addClassPrefix.tsx
+++ src/utils/addClassPrefix.tsx
@@ -1,10 +1,5 @@
 import classNames from "classnames";
 
-let classPrefix = "ts";
-if (process.env.RIGEL_CLASS_PREFIX) {
-  classPrefix = process.env.RIGEL_CLASS_PREFIX;
-}
-
 /**
  * Add prefix for class name.
  *

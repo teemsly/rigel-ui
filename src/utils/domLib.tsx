93ee177a0e35b9cede83ceb7cf8cfd860cd70bf6
mrsepdiana <firdasepdiana@gmail.com>
2021-05-19 10:21:49 +0700

    add chained function, domLib, and overlayClose

---
+++ src/utils/domLib.tsx
@@ -0,0 +1,8 @@
+export * from "dom-lib";
+import * as helpers from "dom-lib";
+
+const domHelper = {
+  ...helpers,
+};
+
+export default domHelper;
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,4 +1,7 @@
 export { default as addClassPrefix } from "./addClassPrefix";
 export * from "./common";
+export { default as createChainedFunction } from "./createChainedFunction";
 export { default as createComponent } from "./createComponent";
+export * from "./domLib";
+export { default as overlayClose } from "./overlayClose";
 export { default as useClassName } from "./useClassName";
